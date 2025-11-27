a3710c5236bf1fc4a16bce73bdea46f5bbc35f41
emiliosdev <sengua2000@outlook.com>
2025-11-27 09:05:20 -0500

    feat: input customer to material

--- apps/demo-app/src/app/modules/company-customer/company-customer-create/company-customer-create.ts
+++ apps/demo-app/src/app/modules/company-customer/company-customer-create/company-customer-create.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CompanyCustomerFormComponent } from '@acontplus/ng-customer';
+import { MatDialogContent, MatDialogActions, MatDialogRef } from '@angular/material/dialog';
+import { Button } from '@acontplus/ng-components';
 
 @Component({
   selector: 'app-company-customer-create',
-  imports: [CompanyCustomerFormComponent],
+  imports: [CompanyCustomerFormComponent, MatDialogContent, MatDialogActions, Button],
   templateUrl: './company-customer-create.html',
 })
 export class CompanyCustomerCreate {
+  private dgRef = inject(MatDialogRef<CompanyCustomerCreate>);
   onCustomerCreated(customer: any): void {
     console.log('Cliente creado:', customer);
     // Mostrar notificación de éxito

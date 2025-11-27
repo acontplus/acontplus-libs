d26171a3f920c5793ae4f32f2614ad3491f9742b
emiliosdev <sengua2000@outlook.com>
2025-11-26 23:56:24 -0500

    feat: improve structure company customer

--- packages/ng-customer/src/lib/data-access/interfaces/customer-service.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface ICustomerService {
-  create(customer: any): Observable<any>;
-  update(id: string, customer: any): Observable<any>;
-  getById(id: string): Observable<any>;
-  search(term: string): Observable<any[]>;
-  validate?(customer: any): Record<string, any> | null;
-}

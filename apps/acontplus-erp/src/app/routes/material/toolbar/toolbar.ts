9e80090c36edcb13a5fdfd34d9a91fa70dde309d
emilios-dev <emilio.senguana.dev@gmail.com>
2026-08-19 13:01:24 -0500

    fix(acontplus-erp): remove duplicate leftActions declaration in toolbar

--- apps/acontplus-erp/src/app/routes/material/toolbar/toolbar.ts
+++ apps/acontplus-erp/src/app/routes/material/toolbar/toolbar.ts
@@ -59,7 +59,6 @@ export class Toolbar {
 
   // Toolbar with spacer
   leftActions = [{ icon: 'menu', tooltip: 'Menu' }];
-  leftActions = [{ icon: 'menu', tooltip: 'Menu' }];
 
   rightActions = [
     { icon: 'search', tooltip: 'Search' },
